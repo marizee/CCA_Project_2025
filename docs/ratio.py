8affed11eb9199b978e1751693b7b2a0e22a7683
marizee <mfly282@gmail.com>
2025-02-10 21:09:05 +0100

    timings and profilers

---
+++ docs/ratio.py
@@ -0,0 +1,31 @@
+import sys
+
+try:
+    file_name = sys.argv[1]
+except IndexError:
+    print("No input file. Running on scalar-vector_25bits_marie_laptop.txt")
+    file_name = "scalar-vector_25bits_marie_laptop.txt"
+
+try:
+    datas = open(file_name,"r").read().splitlines()
+except FileNotFoundError:
+    print("File doesn't exists. Running on scalar-vector_25bits_marie_laptop.txt")
+    datas = open("scalar-vector_25bits_marie_laptop.txt", "r").read().splitlines()
+    fratios = open("ratio_scalar-vector_25bits_marie_laptop.txt", "w")
+else:
+    fratios = open("ratio_" + file_name, "w")
+
+
+parse = datas[4].split()
+fratios.write(f'{parse[0]}\t{parse[2]}\t{parse[3]}\t{parse[4]}\t{parse[5]}\n')
+
+for row in datas[5:]:
+    parse = row.split()
+    ratios = [float(parse[1])/float(parse[i]) for i in range(2,6)]
+    if len(parse[0]) <= 3:
+        fratios.write(f'{parse[0]}\t\t\t')
+    else:
+        fratios.write(f'{parse[0]}\t\t')
+    fratios.write(f'{ratios[0]:.3f}\t\t{ratios[1]:.3f}\t{ratios[2]:.3f}\t{ratios[3]:.3f}\n')
+
+fratios.close()
