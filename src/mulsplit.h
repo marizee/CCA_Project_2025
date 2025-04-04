efed0ea36315551b2379ae6380d1480cbbc01dcb
marizee <mfly282@gmail.com>
2025-04-04 08:20:21 +0200

    update mulsplit

---
+++ src/mulsplit.h
@@ -0,0 +1,23 @@
+#ifndef MULSPLIT_H
+#define MULSPLIT_H
+
+#include <immintrin.h>
+
+#include "flint/flint.h"
+#include "flint/nmod.h"
+#include "flint/nmod_vec.h"
+#include "flint/ulong_extras.h"
+#include "flint/profiler.h"
+
+ulong mulhi_split(ulong a, ulong b) ;
+ulong mullo_split(ulong a, ulong b) ;
+
+__m256i avx2_mulhi_split(__m256i a, __m256i b) ;
+__m256i avx2_mullo_split(__m256i a, __m256i b) ;
+__m256i avx2_mullo_epi64(__m256i a, __m256i b) ;
+
+#if defined(__AVX512__)
+__m512i avx512_mulhi_split(__m512i a, __m512i b) ;
+#endif
+
+#endif
