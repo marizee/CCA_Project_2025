27debc611435d992fe6c1e530cd2d269d75a86ce
marizee <mfly282@gmail.com>
2025-05-15 14:35:20 +0200

    add unrolled scalar vector mod 64

--- src/scalar_vector_mod_64.c
+++ src/scalar_vector_mod_64.c
@@ -47,12 +47,38 @@ void seq_shoup_scalar_vector_mod_64_vectorized(nn_ptr res, ulong w, nn_ptr b, sl
     }
 }
 
-/*
 void seq_shoup_scalar_vector_mod_64_unrolled(nn_ptr res, ulong w, nn_ptr b, slong len, nmod_t mod)
 {
+    ulong p_hi=0, p_lo=0;
+    ulong tmp;
 
+    ulong w_pr = n_mulmod_precomp_shoup(w, mod.n);
+
+    slong i;
+    for (i = 0; i+3 < len; i += 4)
+    {
+        umul_ppmm(p_hi, p_lo, b[i+0], w_pr);
+        tmp = w*b[i+0] - p_hi*mod.n;
+        res[i+0] = (tmp >= mod.n) ? tmp - mod.n : tmp;
+        umul_ppmm(p_hi, p_lo, b[i+1], w_pr);
+        tmp = w*b[i+1] - p_hi*mod.n;
+        res[i+1] = (tmp >= mod.n) ? tmp - mod.n : tmp;
+        umul_ppmm(p_hi, p_lo, b[i+2], w_pr);
+        tmp = w*b[i+2] - p_hi*mod.n;
+        res[i+2] = (tmp >= mod.n) ? tmp - mod.n : tmp;
+        umul_ppmm(p_hi, p_lo, b[i+3], w_pr);
+        tmp = w*b[i+3] - p_hi*mod.n;
+        res[i+3] = (tmp >= mod.n) ? tmp - mod.n : tmp;
+    }
+
+    for ( ; i < len; i++)
+    {
+        umul_ppmm(p_hi, p_lo, b[i], w_pr);
+        tmp = w*b[i] - p_hi*mod.n;
+        res[i] = (tmp >= mod.n) ? tmp - mod.n : tmp;
+    }
 }
-*/
+
 
 void avx2_shoup_scalar_vector_mod_64(nn_ptr res, ulong w, nn_ptr b, slong len, nmod_t mod)
 {
@@ -104,7 +130,92 @@ void avx2_shoup_scalar_vector_mod_64(nn_ptr res, ulong w, nn_ptr b, slong len, n
     }
 }
 
-//void avx2_shoup_scalar_vector_mod_64_unrolled(nn_ptr res, ulong w, nn_ptr b, slong len, nmod_t mod) ;
+void avx2_shoup_scalar_vector_mod_64_unrolled(nn_ptr res, ulong w, nn_ptr b, slong len, nmod_t mod)
+{
+    ulong w_pr = n_mulmod_precomp_shoup(w, mod.n);
+
+    __m256i vw = _mm256_set1_epi64x(w);
+    __m256i vw_pr = _mm256_set1_epi64x(w_pr);
+
+    __m256i vmod = _mm256_set1_epi64x(mod.n);
+
+    __m256i vb, vres;
+
+    slong i;
+    for (i = 0; i+31 < len; i += 32)
+    {
+        vb = _mm256_loadu_si256((const __m256i *)&b[i+0]);
+        vres = _mm256_sub_epi64(avx2_mullo_epi64(vw, vb), avx2_mullo_epi64(avx2_mulhi_split_v2(vw_pr, vb), vmod)); // only low part is needed 
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        _mm256_storeu_si256((__m256i *)&res[i+0], vres);
+        
+        vb = _mm256_loadu_si256((const __m256i *)&b[i+4]);
+        vres = _mm256_sub_epi64(avx2_mullo_epi64(vw, vb), avx2_mullo_epi64(avx2_mulhi_split_v2(vw_pr, vb), vmod)); 
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        _mm256_storeu_si256((__m256i *)&res[i+4], vres);
+        
+        vb = _mm256_loadu_si256((const __m256i *)&b[i+8]);
+        vres = _mm256_sub_epi64(avx2_mullo_epi64(vw, vb), avx2_mullo_epi64(avx2_mulhi_split_v2(vw_pr, vb), vmod)); 
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        _mm256_storeu_si256((__m256i *)&res[i+8], vres);
+        
+        vb = _mm256_loadu_si256((const __m256i *)&b[i+12]);
+        vres = _mm256_sub_epi64(avx2_mullo_epi64(vw, vb), avx2_mullo_epi64(avx2_mulhi_split_v2(vw_pr, vb), vmod)); 
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        _mm256_storeu_si256((__m256i *)&res[i+12], vres);
+
+        vb = _mm256_loadu_si256((const __m256i *)&b[i+16]);
+        vres = _mm256_sub_epi64(avx2_mullo_epi64(vw, vb), avx2_mullo_epi64(avx2_mulhi_split_v2(vw_pr, vb), vmod)); 
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        _mm256_storeu_si256((__m256i *)&res[i+16], vres);
+        
+        vb = _mm256_loadu_si256((const __m256i *)&b[i+20]);
+        vres = _mm256_sub_epi64(avx2_mullo_epi64(vw, vb), avx2_mullo_epi64(avx2_mulhi_split_v2(vw_pr, vb), vmod)); 
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        _mm256_storeu_si256((__m256i *)&res[i+20], vres);
+
+        vb = _mm256_loadu_si256((const __m256i *)&b[i+24]);
+        vres = _mm256_sub_epi64(avx2_mullo_epi64(vw, vb), avx2_mullo_epi64(avx2_mulhi_split_v2(vw_pr, vb), vmod)); 
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        _mm256_storeu_si256((__m256i *)&res[i+24], vres);
+        
+        vb = _mm256_loadu_si256((const __m256i *)&b[i+28]);
+        vres = _mm256_sub_epi64(avx2_mullo_epi64(vw, vb), avx2_mullo_epi64(avx2_mulhi_split_v2(vw_pr, vb), vmod)); 
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        _mm256_storeu_si256((__m256i *)&res[i+28], vres);
+    }
+
+    for ( ; i+3 < len; i+= 4)
+    {
+        vb = _mm256_loadu_si256((const __m256i *)&b[i]);
+        vres = _mm256_sub_epi64(avx2_mullo_epi64(vw, vb), avx2_mullo_epi64(avx2_mulhi_split_v2(vw_pr, vb), vmod)); 
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        vres = _mm256_sub_epi64(vres, _mm256_andnot_si256(_mm256_cmpgt_epi64(vmod, vres), vmod));
+        _mm256_storeu_si256((__m256i *)&res[i], vres);
+    }
+
+    ulong p_hi=0, p_lo=0;
+    ulong tmp;
+
+    for ( ; i<len; i++)
+    {
+        umul_ppmm(p_hi, p_lo, b[i], w_pr);
+        tmp = w*b[i] - p_hi*mod.n;
+        
+        if (tmp >= mod.n)
+            tmp = tmp - mod.n;
+
+        res[i] = tmp;
+    }
+}
 
 #if defined(__AVX512F__)
 void avx512_shoup_scalar_vector_mod_64(nn_ptr res, ulong w, nn_ptr b, slong len, nmod_t mod)
@@ -149,5 +260,63 @@ void avx512_shoup_scalar_vector_mod_64(nn_ptr res, ulong w, nn_ptr b, slong len,
     }
 }
 
-//void avx512_shoup_scalar_vector_mod_64_unrolled(nn_ptr res, ulong w, nn_ptr b, slong len, nmod_t mod) ;
+void avx512_shoup_scalar_vector_mod_64_unrolled(nn_ptr res, ulong w, nn_ptr b, slong len, nmod_t mod)
+{
+    ulong w_pr = n_mulmod_precomp_shoup(w, mod.n);
+
+    __m512i vw = _mm512_set1_epi64(w);
+    __m512i vw_pr = _mm512_set1_epi64(w_pr);
+
+    __m512i vmod = _mm512_set1_epi64(mod.n);
+
+    __m512i vb, vres;
+
+    slong i;
+    for (i = 0; i+31 < len; i += 32)
+    {
+        vb = _mm512_loadu_si512((const __m512i *)&b[i+0]);
+        vres = _mm512_sub_epi64(_mm512_mullo_epi64(vw, vb), _mm512_mullo_epi64(avx512_mulhi_split_v2(vw_pr, vb), vmod));
+        vres = _mm512_min_epu64(_mm512_sub_epi64(vres, vmod), vres);
+        vres = _mm512_min_epu64(_mm512_sub_epi64(vres, vmod), vres);
+        _mm512_storeu_si512((__m512i *)&res[i+0], vres);
+        vb = _mm512_loadu_si512((const __m512i *)&b[i+8]);
+        vres = _mm512_sub_epi64(_mm512_mullo_epi64(vw, vb), _mm512_mullo_epi64(avx512_mulhi_split_v2(vw_pr, vb), vmod));
+        vres = _mm512_min_epu64(_mm512_sub_epi64(vres, vmod), vres);
+        vres = _mm512_min_epu64(_mm512_sub_epi64(vres, vmod), vres);
+        _mm512_storeu_si512((__m512i *)&res[i+8], vres);
+        vb = _mm512_loadu_si512((const __m512i *)&b[i+16]);
+        vres = _mm512_sub_epi64(_mm512_mullo_epi64(vw, vb), _mm512_mullo_epi64(avx512_mulhi_split_v2(vw_pr, vb), vmod));
+        vres = _mm512_min_epu64(_mm512_sub_epi64(vres, vmod), vres);
+        vres = _mm512_min_epu64(_mm512_sub_epi64(vres, vmod), vres);
+        _mm512_storeu_si512((__m512i *)&res[i+16], vres);
+        vb = _mm512_loadu_si512((const __m512i *)&b[i+24]);
+        vres = _mm512_sub_epi64(_mm512_mullo_epi64(vw, vb), _mm512_mullo_epi64(avx512_mulhi_split_v2(vw_pr, vb), vmod));
+        vres = _mm512_min_epu64(_mm512_sub_epi64(vres, vmod), vres);
+        vres = _mm512_min_epu64(_mm512_sub_epi64(vres, vmod), vres);
+        _mm512_storeu_si512((__m512i *)&res[i+24], vres);
+    }
+
+    for ( ; i+7 < len; i += 8)
+    {
+        vb = _mm512_loadu_si512((const __m512i *)&b[i]);
+        vres = _mm512_sub_epi64(_mm512_mullo_epi64(vw, vb), _mm512_mullo_epi64(avx512_mulhi_split_v2(vw_pr, vb), vmod));
+        vres = _mm512_min_epu64(_mm512_sub_epi64(vres, vmod), vres);
+        vres = _mm512_min_epu64(_mm512_sub_epi64(vres, vmod), vres);
+        _mm512_storeu_si512((__m512i *)&res[i], vres);
+    }
+
+    ulong p_hi=0, p_lo=0;
+    ulong tmp;
+
+    for ( ; i<len; i++)
+    {
+        umul_ppmm(p_hi, p_lo, b[i], w_pr);
+        tmp = w*b[i] - p_hi*mod.n;
+        
+        if (tmp >= mod.n)
+            tmp = tmp - mod.n;
+
+        res[i] = tmp;
+    }
+}
 #endif
--- src/tests/test_sc_mod_64.c
+++ src/tests/test_sc_mod_64.c
@@ -12,15 +12,15 @@
 
 int main()
 {
-    slong len = 10;
-    flint_bitcnt_t bits = 5;
+    slong len = 1 << 20;
+    flint_bitcnt_t bits = 60;
 
     FLINT_TEST_INIT(state);
 
     nmod_t mod;
     ulong n, w;
-    nn_ptr b, res1, res2, res3;//, res4;
-    nn_ptr res5;//, res6;
+    nn_ptr b, res1, res2, res3, res4;
+    nn_ptr res5, res6;
 
     // init modulus structure
     n = n_randbits(state, (uint32_t)bits);
@@ -34,9 +34,9 @@ int main()
     res1 = _nmod_vec_init(len);
     res2 = _nmod_vec_init(len);
     res3 = _nmod_vec_init(len);
-    //res4 = _nmod_vec_init(len);
+    res4 = _nmod_vec_init(len);
     res5 = _nmod_vec_init(len);
-    //res6 = _nmod_vec_init(len);
+    res6 = _nmod_vec_init(len);
 
     // init scalar
     w = n_randint(state, n);
@@ -51,14 +51,14 @@ int main()
     
     // tests
     clock_t start, end;
-    double tflint, tseq, tseqv;//, tseq_unr;
-    double tavx2;//, tavx2_unr;
+    double tflint, tseq, tseqv, tseq_unr;
+    double tavx2, tavx2_unr;
 
     start = clock();
     flint_shoup_scalar_vector_mod_64(res1,w,b,len,mod);
     end = clock();
     tflint = ((double) (end - start)) / CLOCKS_PER_SEC;
-    _nmod_vec_print_pretty(res1, len, mod);
+    //_nmod_vec_print_pretty(res1, len, mod);
     printf("tflint=\t\t%.5es\n", tflint);
 
     start = clock();
@@ -75,14 +75,12 @@ int main()
     //_nmod_vec_print_pretty(res3, len, mod);
     printf("seqv=\t\t%.5es\n", tseqv);
 
-/*
     start = clock();
     seq_shoup_scalar_vector_mod_64_unrolled(res4,w,b,len,mod);
     end = clock();
     tseq_unr = ((double) (end - start)) / CLOCKS_PER_SEC;
-    _nmod_vec_print_pretty(res4, len, mod);
+    //_nmod_vec_print_pretty(res4, len, mod);
     printf("unr=\t\t%.5es\n", tseq_unr);
-*/
 
     start = clock();
     avx2_shoup_scalar_vector_mod_64(res5,w,b,len,mod);
@@ -90,53 +88,55 @@ int main()
     //_nmod_vec_print_pretty(res5, len, mod);
     tavx2 = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("avx2=\t\t%.5es\n", tavx2);
-/*
+
     start = clock();
-    simd2_mod_scalar_vector_unrolled(res6, b, vec, len);
-    //_nmod_vec_print_pretty(res4, len, mod);
+    avx2_shoup_scalar_vector_mod_64_unrolled(res6,w,b,len,mod);
+    //_nmod_vec_print_pretty(res6, len, mod);
     end = clock();
     tavx2_unr = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("avx2_unr=\t%.5es\n", tavx2_unr);
     
-*/
 
 #if defined(__AVX512F__)
-    nn_ptr res7;//, res8;
+    nn_ptr res7, res8;
     res7 = _nmod_vec_init(len);
-    //res8 = _nmod_vec_init(len);
+    res8 = _nmod_vec_init(len);
 
-    double tavx512;//, tavx512_unr;
+    double tavx512, tavx512_unr;
 
     start = clock();
     avx512_shoup_scalar_vector_mod_64(res7,w,b,len,mod);
     end = clock();
-    _nmod_vec_print_pretty(res7, len, mod);
+    //_nmod_vec_print_pretty(res7, len, mod);
     tavx512 = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("avx512=\t\t%.5es\n", tavx512);
-/*
+
     start = clock();
     simd2_mod_scalar_vector_unrolled(res8, b, vec, len);
     //_nmod_vec_print_pretty(res8, len, mod);
     end = clock();
     tavx512_unr = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("avx512_unr=\t%.5es\n", tavx512_unr);
-*/
+
 #endif
 
     // checks
     int s1 = _nmod_vec_equal(res1, res2, len);
     int s2 = _nmod_vec_equal(res1, res3, len);
+    int s3 = _nmod_vec_equal(res1, res4, len);
     
-    int v2 = _nmod_vec_equal(res1, res5, len);
+    int v2_1 = _nmod_vec_equal(res1, res5, len);
+    int v2_2 = _nmod_vec_equal(res1, res6, len);
 
 #if defined(__AVX512F__)
-    int v512 = _nmod_vec_equal(res1, res7, len);
+    int v512_1 = _nmod_vec_equal(res1, res7, len);
+    int v512_2 = _nmod_vec_equal(res1, res8, len);
 
-    if (!s1 || !s2 || !v2 || !v512)
-        printf("ff - s1=%d s2=%d v2=%d v512=%d\n", s1, s2, v2, v512);
+    if (!s1 || !s2 || !s3 || !v2_1 || !v2_2 || !v512_1 || !v512_2)
+        printf("ff - s1=%d s2=%d s3=%d v2_1=%d v2_2=%d v512_1=%d v512_2=%d\n", s1, s2, s3, v2_1, v2_2, v512_1, v512_2);
 #else
-    if (!s1 || !s2 || !v2)
-        printf("ff - s1=%d s2=%d v2=%d\n", s1, s2, v2);
+    if (!s1 || !s2 || !s3 || !v2_1 || !v2_2)
+        printf("ff - s1=%d s2=%d s3=%d v2_1=%d v2_2=%d\n", s1, s2, s3, v2_1, v2_2);
 #endif
     else 
         printf("OK!\n");
@@ -145,12 +145,12 @@ int main()
     _nmod_vec_clear(res1);
     _nmod_vec_clear(res2);
     _nmod_vec_clear(res3);
-    //_nmod_vec_clear(res4);
+    _nmod_vec_clear(res4);
     _nmod_vec_clear(res5);
-    //_nmod_vec_clear(res6);
+    _nmod_vec_clear(res6);
 #if defined(__AVX512F__)
     _nmod_vec_clear(res7);
-    //_nmod_vec_clear(res8);
+    _nmod_vec_clear(res8);
 #endif
     FLINT_TEST_CLEAR(state);
     return 0;
