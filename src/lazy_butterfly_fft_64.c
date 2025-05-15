bf197cabe1349a5b29f13351d65bc1fcec0c20a9
marizee <mfly282@gmail.com>
2025-05-15 13:34:48 +0200

    correction modulo in lazy

--- src/lazy_butterfly_fft_64.c
+++ src/lazy_butterfly_fft_64.c
@@ -35,6 +35,8 @@ void avx2_preinv_split_fft_lazy44(nn_ptr a, nn_ptr b, ulong w, ulong w_pr, slong
     __m256i vmod = _mm256_set1_epi64x(n);
     __m256i vmod2 = _mm256_set1_epi64x(n2);
 
+    __m256i cmp, mask;
+
     slong i;
     for (i=0; i+3<len; i+=4)
     {
@@ -42,8 +44,8 @@ void avx2_preinv_split_fft_lazy44(nn_ptr a, nn_ptr b, ulong w, ulong w_pr, slong
         __m256i vb = _mm256_loadu_si256((const __m256i *)(b+i));
 
         // (a[i] < n2) ? a[i] : a[i] - n2
-        __m256i cmp = _mm256_cmpgt_epi64(vmod2, va);
-        __m256i mask = _mm256_andnot_si256(cmp, vmod2);
+        cmp = _mm256_cmpgt_epi64(vmod2, va);
+        mask = _mm256_andnot_si256(cmp, vmod2);
         va = _mm256_sub_epi64(va, mask);
 
         __m256i vq_hi = avx2_mulhi_split_v2(vw_pr, vb);
@@ -51,7 +53,12 @@ void avx2_preinv_split_fft_lazy44(nn_ptr a, nn_ptr b, ulong w, ulong w_pr, slong
         __m256i llo, rlo;
         llo = avx2_mullo_epi64(vw, vb);
         rlo = avx2_mullo_epi64(vq_hi, vmod);
-        __m256i vres = _mm256_sub_epi64(llo, rlo); // only low part is needed 
+        __m256i vres = _mm256_sub_epi64(llo, rlo); // only low part is needed
+
+        // vres either ok or +n or +2n => needs 1 mod red
+        cmp = _mm256_cmpgt_epi64(vmod2, vres);
+        mask = _mm256_andnot_si256(cmp, vmod2);
+        vres = _mm256_sub_epi64(vres, mask);
 
         __m256i add = _mm256_add_epi64(va, vres);
         __m256i sub = _mm256_add_epi64(_mm256_sub_epi64(va, vres), vmod2);
@@ -96,9 +103,6 @@ void avx512_preinv_split_fft_lazy44(nn_ptr a, nn_ptr b, ulong w, ulong w_pr, slo
         __m512i vb = _mm512_loadu_si512((const __m512i *)(b+i));
 
         // (a[i] >= n2) ? a[i] - n2 : a[i] <=> min(a[i] - 2n, a[i])
-        //__mmask8 mask = _mm512_cmpge_epi64_mask(va, vmod2); // 1111111 if a[i] >= n2, 0 else
-        //__m512i tmp2 = _mm512_maskz_set1_epi64(mask, n2);
-        //va = _mm512_sub_epi64(va, tmp2);
 	    va = _mm512_min_epu64(_mm512_sub_epi64(va, vmod2), va);
 
         __m512i vq_hi = avx512_mulhi_split_v2(vw_pr, vb);
@@ -107,6 +111,8 @@ void avx512_preinv_split_fft_lazy44(nn_ptr a, nn_ptr b, ulong w, ulong w_pr, slo
         llo = _mm512_mullo_epi64(vw, vb);
         rlo = _mm512_mullo_epi64(vq_hi, vmod);
         __m512i vres = _mm512_sub_epi64(llo, rlo);
+
+	    vres = _mm512_min_epu64(_mm512_sub_epi64(res, vmod2), vres);
 
         __m512i add = _mm512_add_epi64(va, vres);
         __m512i sub = _mm512_add_epi64(_mm512_sub_epi64(va, vres), vmod2);
